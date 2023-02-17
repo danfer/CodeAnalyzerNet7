4c80f04c49b10b00daac97ae464a37e02e8ac340
Danfer Habed <dhabed@outlook.com>
2023-02-17 05:38:14 -0600

    Add project files.

---
+++ CodeAnalizing/Program.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="Program.cs" company="HabedSolutions">
+// Copyright (c) HabedSolutions. All rights reserved.
+// </copyright>
+namespace CodeAnalyzing;
+using System.Diagnostics;
+
+/// <summary>
+/// The main class for this console app.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// The main entry point for this console app.
+    /// </summary>
+    /// <param name="args">
+    /// A string array of arguments passed to the console app.
+    /// </param>
+    public static void Main(string[] args)
+    {
+        Debug.WriteLine("Hello, Debugger!");
+    }
+}
